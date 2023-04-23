948231d5a37f5570f4486bd0533a3467762711c4
infandis <svetlana290699@gmail.com>
2023-04-23 14:45:02 +0300

    redacted homework

--- main.js
+++ main.js
@@ -1,38 +1,21 @@
-const emojis = [
-    "🍔",
-    "🍕",
-    "🍣",
-    "🍜",
-    "🍩",
-  ];
+const emojisContainer = document.querySelector("#emoji-vote .emojis");
+    const voteCountContainers = document.querySelectorAll("#emoji-vote .vote-count");
+    const voteCounts = new Array(voteCountContainers.length).fill(0);
 
-let voteCounts = new Array(emojis.length).fill(0);
-function vote(index) {
-  voteCounts[index]++;
-  renderVoteResults();
-}
+    function vote(event) {
+      const emoji = event.target;
+      const emojiIndex = Array.from(emojisContainer.children).indexOf(emoji);
+      voteCounts[emojiIndex]++;
+      renderVoteResults();
+    }
 
+    function renderVoteResults() {
+      for (let i = 0; i < voteCountContainers.length; i++) {
+        const voteCountContainer = voteCountContainers[i];
+        voteCountContainer.innerText = voteCounts[i];
+      }
+    }
 
-function renderVoteResults() {
-  const emojisContainer = document.querySelector("#emoji-vote .emojis");
-  const voteCountContainer = document.querySelector("#emoji-vote .vote-count");
-  
-  emojisContainer.innerHTML = "";
-  voteCountContainer.innerHTML = "";
-
-  emojis.forEach((emoji, index) => {
-    const emojiElem = document.createElement("div");
-    emojiElem.innerText = emoji;
-    emojiElem.classList.add("emoji");
-    emojiElem.addEventListener("click", () => {
-      vote(index);
-    });
-    emojisContainer.appendChild(emojiElem);
+    emojisContainer.addEventListener("click", vote);
+    renderVoteResults();
     
-    const voteCountElem = document.createElement("div");
-    voteCountElem.innerText = voteCounts[index];
-    voteCountContainer.appendChild(voteCountElem);
-  });
-}
-
-renderVoteResults();
